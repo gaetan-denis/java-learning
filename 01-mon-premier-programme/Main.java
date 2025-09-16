b57e83c0aab1004f9af479bdb2fc776c9893032c
Gaëtan Denis <gaetandenis@outlook.be>
2025-09-16 19:21:37 +0200

    feat: add first Java program "Hello World"
    Includes a basic structure:
    - public class
    - main method
    - System.out.println() for console output

---
+++ 01-mon-premier-programme/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Structure de base d'un programme java : 
+ * - Déclaration d'une classe publique, le nom de cette classe doit être strictement identique au nom du fichier à compiler.
+ * - Méthode main() : point d'entrée du programme
+ * - System.out.println() : affiche du texte à la console, (raccourci 'sout' pour autocomplétion)
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
