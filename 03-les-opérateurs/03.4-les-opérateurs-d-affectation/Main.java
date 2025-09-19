95e9589bfb1b30b841818f9d21b6920ec993a760
Gaëtan Denis <gaetandenis@outlook.be>
2025-09-19 21:34:32 +0200

    feat: add example for += assignment operator

---
+++ 03-les-opérateurs/03.4-les-opérateurs-d-affectation/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args){
+        
+
+        // L'opérateur d'affectation
+
+        int monPremierEntier = 42; 
+
+        System.out.println("Mon premier entier : " + monPremierEntier);
+
+        /**
+         * opérateur d’affectation par addition:
+         * - Ajoute la valeur de droite à la variable de gauche.
+         * - Réaffecte le résultat à cette variable.
+         */
+
+        monPremierEntier += 12;
+
+        System.out.println("Mon premier entier : " + monPremierEntier);
+    }
+}
