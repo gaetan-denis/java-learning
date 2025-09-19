db50dda8499c6e9b11b9328a1dba382d79d50d1a
Gaëtan Denis <gaetandenis@outlook.be>
2025-09-19 20:21:25 +0200

    refactor: move even-check to isEvenHeight method and correct pair/impair wording

--- 03-les-opérateurs/03.3-exercice-opérateurs-arithmétiques-modulo/Main.java
+++ 03-les-opérateurs/03.3-exercice-opérateurs-arithmétiques-modulo/Main.java
@@ -8,11 +8,15 @@ public class Main {
         System.out.print("Hauteur de la montagne : ");
         int hauteurDeLaMontagne = Integer.parseInt(scanner.nextLine());
         System.out.println("Le nom de la montagne est : " + nomDeLaMontagne + " et sa hauteur cumule à : " + hauteurDeLaMontagne + "m");
-        if(hauteurDeLaMontagne %2 == 0){
-            System.out.println("La hauteur de la montagne est une valeur pair");
+        if(isEvenHeight(hauteurDeLaMontagne)){
+            System.out.println("La hauteur de la montagne est une valeur paire");
         }else {
-            System.out.println("La valeur de la montagne est une valeur impair");
+            System.out.println("La valeur de la montagne est une valeur impaire");
         }
         scanner.close();
+    }
+
+    public static boolean isEvenHeight(int value){
+        return value %2 == 0;
     }
 }
