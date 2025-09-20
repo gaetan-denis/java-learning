c350946b4b5aee9f8201d08048e204e8e375eb5f
Gaëtan Denis <gaetandenis@outlook.be>
2025-09-21 01:13:41 +0200

    featt: add exercise using if-else conditional structure
    - Declare an integer `monEntier`
    - Print its value
    - Use if-else statements to determine if the integer is positive,
      negative, or zero

---
+++ 04-les-structures/04.3-structure-if-else/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int monEntier = 2;
+        System.out.println("La valeur de cet entier est " + monEntier);
+
+        if (monEntier > 0) {
+            System.out.println("Mon entier est positif");
+        } else if (monEntier < 0) {
+            System.out.println("Mon entier est négatif");
+        } else {
+            System.out.println("Mon entier est null");
+        }
+
+    }
+}
