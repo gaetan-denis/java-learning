b46ffe6c5ab47060713e60d3ed8f776bb473131e
Gaëtan Denis <gaetandenis@outlook.be>
2025-10-02 21:47:09 +0200

    chore: practice OOP in Java with Terrain class exercise

---
+++ 07-la-Programmation-Orientée-Objet-création-de-classes/07.01-les-base-de-la-Programmation-Orientée-Objet/Terrain.java
@@ -0,0 +1,36 @@
+// Classe TErrain qui définit un terrain
+public class Terrain{
+    
+    // Un terrain, qu'est-ce que c'est? --> Attributs
+
+    int largeur;
+    int longueur;
+
+    // Un terrain ça se construit comment? --> Constructeurs
+    
+    
+
+    Terrain(int paramLargeur, int paramLongueur){
+        largeur = paramLargeur;
+        longueur = paramLongueur;
+    }
+
+    // Un terrain, qu'est-ce que ça sait faire? --> Méthodes
+
+    int superficie(){
+        int laSuperficieDuTerrain = largeur * longueur;
+        return laSuperficieDuTerrain;
+    }
+
+    void afficher(){
+        System.out.println("Terrain de taille : " + largeur + " x " + longueur +", superficie : " + superficie() + "m²");
+    }
+
+    boolean aUneSuperficiePlusGrandeQue (int valeur){
+        if(superficie() > valeur){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
